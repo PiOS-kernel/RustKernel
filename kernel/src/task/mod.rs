4004eebdea2124262fdaf9e1be201b81e4f02194
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2022-11-14 02:43:14 +0100

    added a `LockedQueue`and some utilities methods for the TCBs

--- kernel/src/task/mod.rs
+++ kernel/src/task/mod.rs
@@ -1,13 +1,13 @@
 use core::ptr;
 use alloc::boxed::Box;
 use core::marker::Sync;
+use crate::{utility::memcpy, mutex::Mutex};
 
 //file to be reviewed, probably need to split it into modules, probably need to address some details
 
 //defined type 
 type TcbBlock = Option<Box<TaskTCB>>;           //used as a reference to a Task_TCB 
 const STACK_SIZE: usize = 1024;                         //size of the stack for every task
-type StPointer = Option<Box<usize>>;            //stack pointer
 
 //global variables
 pub const MAX_PRIORITY: u8 = 10;                    //max priority and size of the priority queues array
@@ -17,7 +17,7 @@ static mut RUNNING: TcbBlock = None;                       //variable for the ru
 
 pub struct TaskTCB {
     next: TcbBlock,                     //reference to the next Task_TCB
-    priority: u8,                       //priority of the task
+    pub priority: u8,                       //priority of the task
     stp: usize,                         //stack pointer
     stack: [u8; STACK_SIZE],            //stack associated to the task
 }
@@ -28,6 +28,56 @@ impl TaskTCB {
     //with the associating the parameters to the corresponding fields
     pub fn new (n: TcbBlock, p: u8) -> Self{    
         Self { next: n, priority: p, stp: 0, stack: [0; STACK_SIZE] }
+    }
+
+    // utility method to push values onto the task's stack
+    pub fn stack_push(&mut self, ptr: *mut u8, size: usize) {
+        // Check whether there is room left on the stack
+        if self.stp > STACK_SIZE - size {
+            panic!();
+        }
+
+        // The data is stored onto the stack and the stack pointer
+        // is incremented.
+        unsafe {
+            let base = (&mut self.stack) as *mut u8;
+            memcpy(ptr, base.add(self.stp), size);
+        }
+        self.stp += size;
+    }
+
+    // this method allows to access the task's stack pointer as a raw memory
+    // address
+    pub fn get_stp(&self) -> *mut u8 {
+        unsafe{
+            let base = &self.stack as *const u8 as *mut u8;
+            base.add(self.stp)
+        }
+    }
+}
+
+/*
+This struct is simply a wrapper to the `Queue` struct,
+it uses a mutex to encapsulate the queue.
+It is necessary because the queue will be declared as 
+static and because of rust rules it will be necessary 
+to get a `&mut` reference out of a `&` reference.
+*/
+pub struct LockedQueue {
+    mux: Mutex<Queue>
+}
+
+impl LockedQueue {
+    pub const fn new() -> Self {
+        Self {mux: Mutex::new(Queue::new())}
+    }
+    pub fn enqueue(&self, block: TaskTCB) {
+        let mut queue = self.mux.lock();
+        queue.enqueue(block);
+    }
+    pub fn dequeue(&self) -> Option<TaskTCB> {
+        let mut queue = self.mux.lock();
+        queue.dequeue()
     }
 }
 
