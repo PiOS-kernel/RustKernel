19c1c5ad5828b951c1ee5b8ba820babee3ace954
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2022-11-24 18:06:06 +0100

    some new test for the heap

--- test_app/src/alloc_tests.rs
+++ test_app/src/alloc_tests.rs
@@ -1,6 +1,7 @@
 use kernel::allocator::{Heap};
 use cortex_m_semihosting::hprintln;
 
+/* Utility function to display the free segments present in the heap */
 fn print_heap(heap: Heap) {
     hprintln!();
     for seg in heap.iter() {
@@ -9,11 +10,46 @@ fn print_heap(heap: Heap) {
 }
 
 #[test_case]
-fn trivial_assertion() {
+fn heap_init_test() {
     let heap_mem: [u8; 1024] = [0; 1024];
     let mut heap = Heap::new();
-    heap.init(unsafe{&heap_mem as *const [u8] as *const u8 as usize}, 1024);
+    heap.init(&heap_mem[0] as *const u8 as usize, 1024);
+    assert_eq!(heap.available_space(), 1024);
+    assert_eq!(heap.count_segments(), 1);
+}
 
-    print_heap(heap);
-    assert_eq!(1, 1);
+#[test_case] 
+fn count_segments_test() {
+    let heap_mem: [u8; 1024] = [0; 1024];
+    let mut heap = Heap::new();
+    
+    for i in 0..50 {
+        let address = &heap_mem[20 * i] as *const u8 as usize;
+        heap.add_free_segment(address, 20);
+        assert_eq!(heap.count_segments(), i + 1);
+    }
+}
+
+#[test_case]
+fn available_space_test() {
+    let heap_mem: [u8; 1024] = [0; 1024];
+    let mut heap = Heap::new();
+    heap.init(&heap_mem[0] as *const u8 as usize, 1024);
+    
+    for i in 0..50 {
+        heap.allocate_segment(20);
+        assert_eq!(heap.available_space(), 1024 - (i + 1) * 20);
+    }
+}
+
+#[test_case]
+fn heap_compaction_test() {
+    let heap_mem: [u8; 1024] = [0; 1024];
+    let mut heap = Heap::new();
+
+    let mut address = &heap_mem[0] as *const u8 as usize;
+    heap.add_free_segment(address, 128);
+    assert_eq!(heap.count_segments(), 1);
+
+    // TODO
 }
