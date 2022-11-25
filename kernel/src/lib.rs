e7d0d18a57a170f40873ffbd343dcea7ca33c91a
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2022-11-25 11:31:39 +0100

    the heap memory is allocated statically. forced HEAP and WAITING_QUEUE to be allocated in RAM

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -12,13 +12,23 @@ pub mod utility;
 use allocator::LockedHeap;
 use task::LockedQueue;
 
-// The start address and size for the kernel's heap
-pub const HEAP_START: usize = 0x2008000; // The middle of RAM's address space
+// The memory for the heap is allocated at compile time
+static mut HEAP_MEMORY: [u8; 0x8000] = [0; 0x8000];
 pub const HEAP_SIZE: usize = 0x8000; // 32KB
 
-// The kernel's heap
+// The kernel's heap and the task queue
+// The `mut` keyword prevents the linker from placing those variables in FLASH
+// memory, as it would assume them to be read-only. If that was the case,
+// mutating those objects would silently fail.
 #[global_allocator]
-pub static HEAP: LockedHeap = LockedHeap::new();
+static mut heap: LockedHeap = LockedHeap::new();
+pub static HEAP: &LockedHeap = unsafe{&heap};
+static mut waiting_queue: LockedQueue = LockedQueue::new();
+pub static WAITING_QUEUE: &LockedQueue = unsafe{&waiting_queue};
 
-// The tasks queue
-pub static WAITING_QUEUE: LockedQueue = LockedQueue::new();
+// The kernel initialization routine, for the time being it just 
+// initializes the heap
+pub unsafe fn kernel_init() {
+    let heap_start = &HEAP_MEMORY[0] as *const u8 as usize;
+    HEAP.init(heap_start, HEAP_SIZE);
+}
