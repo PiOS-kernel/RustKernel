a0cc20095e87880caab4683689fde3631fcce69b
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2022-11-27 10:08:52 +0100

    extended test_create_task()

--- test_app/src/syscalls_tests.rs
+++ test_app/src/syscalls_tests.rs
@@ -1,4 +1,5 @@
-use kernel::syscalls::create_task;
+
+use kernel::{syscalls::create_task, WAITING_QUEUE};
 
 fn foo(pippo : *mut u8) -> ! {
     loop {
@@ -8,5 +9,22 @@ fn foo(pippo : *mut u8) -> ! {
 
 #[test_case]
 fn test_create_task() {
-    create_task(foo, 0 as *mut u8, 0);
+    let args_ptr = 0;
+    create_task(foo, unsafe{ args_ptr as *mut u8 }, 0);
+    assert_eq!(WAITING_QUEUE.count_tasks(), 1);
+
+    let mut created_task = WAITING_QUEUE.dequeue().unwrap();
+    // r0 should contain the pointer to the tasks arguments
+    let r0_ptr = unsafe{ &created_task.stack[0] as *const u8 as *const usize };
+    assert_eq!(unsafe{ *r0_ptr }, args_ptr);
+    
+    // registers r1 - r14 should be 0-filled
+    for i in 1..15 {
+        let reg_ptr = unsafe{ r0_ptr.add(i) };
+        assert_eq!(unsafe{ *reg_ptr }, 0);
+    }
+
+    // r15(pc) should contain the pointer to the task function
+    let r15_ptr = unsafe{ r0_ptr.add(15) };
+    assert_eq!(unsafe{ r15_ptr }, foo as *const usize);
 }
