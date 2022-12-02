7346633ddbd8b5222dbeafe6738c70a997d20a04
AlbertoZaupa <alberto.zaupa@studenti.unitn.it>
2022-12-02 23:55:21 +0100

    fixed misstypes and refactored some more tests

--- test_app/src/task_tests.rs
+++ test_app/src/task_tests.rs
@@ -1,6 +1,6 @@
 use cortex_m_semihosting::hprintln;
 use kernel::{HEAP};
-use kernel::task::{Queue, TaskTCB};
+use kernel::task::{Queue, TaskTCB, STACK_SIZE};
 use alloc::boxed::Box;
 
 #[test_case]
@@ -14,7 +14,7 @@ fn test_queue_empty() {
 fn test_enqueue() {
     let mut queue = Queue::new();
 
-    for i in 0..10 {
+    for i in 0..5 {
         let new_task = Box::new(TaskTCB::new(None, i));
         queue.enqueue(new_task);
         assert_eq!(queue.count_tasks(), (i + 1) as usize);
@@ -25,13 +25,13 @@ fn test_enqueue() {
 fn test_dequeue() {
     let mut queue = Queue::new();
 
-    for i in 0..10 {
+    for i in 0..5 {
         let new_task = Box::new(TaskTCB::new(None, i));
         queue.enqueue(new_task);
         assert_eq!(queue.count_tasks(), (i + 1) as usize);
     }
 
-    for i in 0..10 {
+    for i in 0..5 {
         let task = queue.dequeue().unwrap();
         assert_eq!(task.priority, i);
         assert_eq!(queue.count_tasks(), (10 - i - 1) as usize);
@@ -41,22 +41,29 @@ fn test_dequeue() {
 #[test_case]
 fn test_stack_push() {
     let mut task_tcb = TaskTCB::new(None, 0);
-    let base = (&mut task_tcb.stack) as *mut u8;
     let mut buff: [u8; 5] = [1, 2, 3, 4, 5];
     let src = (&mut buff[0]) as *mut u8;
     let stp_old = task_tcb.stp;
 
     task_tcb.stack_push(src,5);
 
+
+    let base = task_tcb.stp;
     for i in 0..5 {
         unsafe { assert_eq!(*base.offset(i as isize) , *src.offset(i as isize))}
     }
 
-    assert_eq!(stp_old, unsafe{ task_tcb.stp.sub(5) });
+    assert_eq!(stp_old, unsafe{ task_tcb.stp.add(5) });
 }
 
 #[test_case]
-fn test_stack_base() {
+fn test_stack_start() {
     let mut task_tcb = TaskTCB::new(None, 0);
-    assert_eq!(task_tcb.stack_base(), unsafe{ &mut task_tcb.stack[0] as *mut u8});
+    assert_eq!(task_tcb.stack_start(), unsafe{ &mut task_tcb.stack[0] as *mut u8});
+}
+
+#[test_case]
+fn test_stack_end() {
+    let mut task_tcb = TaskTCB::new(None, 0);
+    assert_eq!(task_tcb.stack_end(), unsafe{ (&mut task_tcb.stack[0] as *mut u8).add(STACK_SIZE)});
 }
